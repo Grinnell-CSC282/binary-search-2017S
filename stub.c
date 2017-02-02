1d919ddabb36478a320cf0d497b0793f1213a031
David Kreis <kreisdavid@gmail.com>
2017-02-02 08:55:19 -0600

    dave

--- stub.c
+++ stub.c
@@ -18,29 +18,5 @@
 int
 search (int val, int *vals, int n)
 {
-  if (val == vals[n/2]){
-		return n/2;
-	}
-	else if (val < vals[n/2]){
-		return search_Helper (val, vals, 0, n/2-1);
-	}
-	else {
-		return search_Helper (val, vals, n/2+1, n);
-	}
+  return -1;    // STUB
 } // search
-
-int 
-search_Helper (int val, int *vals, int start, int end){
-	if (start == end){
-		return -1;
-	}
-	if (val == vals[end/2]){
-		return end/2;
-	}
-	else if (val < vals[end/2]){
-		return search_Helper (val, vals, start, end/2-1);
-	}
-	else {
-		return search_Helper (val, vals, end/2+1, end);
-	}
-}
