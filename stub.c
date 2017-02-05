de71c9dfaf79aee7772afea6acf18c47adde392b
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2017-02-05 12:54:11 -0600

    Restored the original linear-search.c and stub.c.

---
+++ stub.c
@@ -0,0 +1,22 @@
+/**
+ * USERID.c
+ *   Binary search on arrays of integers, implemented by YOUR NAME.
+ *
+ * <insert appropriate FOSS license>
+ */
+
+// +---------+-------------------------------------------------------
+// | Headers |
+// +---------+
+
+#include "search.h"
+
+// +--------------------+--------------------------------------------
+// | Exported Functions |
+// +--------------------+
+
+int
+search (int val, int *vals, int n)
+{
+  return -1;    // STUB
+} // search
