77f731737689ba054d404ba8d9d129901931cdd7
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2017-02-05 12:57:36 -0600

    Clean up after students.

--- check.c
+++ check.c
@@ -1,6 +1,7 @@
 /**
  * check.c
- *   A few quick checks for a search in ordered array algorithm.
+ *   A few quick checks for an algorithm that searches in an 
+ *   ordered array.
  *
  * <insert appropriate FOSS license>
  */
@@ -12,6 +13,7 @@
 #include "search.h"
 #include <stdio.h>
 
+
 // +------+----------------------------------------------------------
 // | Main |
 // +------+
@@ -21,52 +23,30 @@ main (int argc, char *argv[])
 {
   int errors = 0;       // A count of errors in the code
   int vals[] = { 3, 7, 8, 11, 12, 20, 21 };     // Values to search
+  int i;                // Counter variable for loops
 
-  // Do some checks
-  if (3 != search (11, vals, 7))
+  // Check every position in the array
+  for (i = 0; i < 7; i++)
     {
-      fprintf (stderr, "Search for 11 failed!\n");
-      ++errors;
-    }
-  if (1 != search (7, vals, 7))
-    {
-      fprintf (stderr, "Search for 7 failed!\n");
-      ++errors;
-    }
-  if (5 != search (20, vals, 7))
-    {
-      fprintf (stderr, "Search for 20 failed!\n");
-      ++errors;
-    }
-  if (-1 != search(1000, vals, 7))
-  {
-    fprintf(stderr, "Search for 1000 failed!\n");
-    ++errors;
-  }
-  for (int i = 0; i < 7; i++)
-  {
       if (i != search(vals[i], vals, 7))
-      {
-        fprintf(stderr, "Search for %d  failed!\n", vals[i]);
-        ++errors;
-      }
-  }
+        {
+          fprintf(stderr, "Search for %d  failed!\n", vals[i]);
+          ++errors;
+        }
+    } // for
 
-  if (6 != search (21, vals, 7))
-    {
-      fprintf (stderr, "Search for 21 failed!\n");
-      ++errors;
-    }
-   if (0 != search (3, vals, 7))
-    {
-      fprintf (stderr, "Search for 3 failed!\n");
-      ++errors;
-    }
-    if (-1 != search (10, vals, 7))
+  // Check for a few values not in the array
+  if (-1 != search (10, vals, 7))
     {
       fprintf (stderr, "Search for 10 failed!\n");
       ++errors;
     }
+  if (-1 != search(1000, vals, 7))
+    {
+      fprintf(stderr, "Search for 1000 failed!\n");
+      ++errors;
+    }
+
   // Report on total number of errors.
   if (!errors)
     {
