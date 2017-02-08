5f2764d41c70df8ce4acd5d8566838133e4f970d
Higinio Jasso <jassohig17@cs.grinnell.edu>
2017-02-08 17:00:42 -0600

    Commented and formatted

--- nelsonhe17jassohig17.c
+++ nelsonhe17jassohig17.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
+// checks if search value was found
 int
 outcheck (int prev, int val)
 {
   if (val == -1)
     return -1;
   else
-    return prev+val;
-}
+    return prev + val;
+}                               // outcheck
 
+// searches for int val in sorted array vals of length n
 int
 search (int val, int *vals, int n)
 {
+  // checks the size of the array
   if (n < 2)
     {
+      // checks if size of array is 1
       if (n == 1)
         {
           if (vals[0] == val)
@@ -24,16 +28,17 @@ search (int val, int *vals, int n)
         }
       else
         return -1;
-    }
+    }                           // if (outermost)
   else
     {
-      int halfint = (int) floor (((double) n)/2);
-  
+      int halfint = (int) floor (((double) n) / 2);     // half array length
+
+      // recursively calls on proper half of array
       if (vals[halfint] > val)
-        return search (val,vals,halfint);
+        return search (val, vals, halfint);
       else if (vals[halfint] < val)
-        return outcheck (halfint, search (val, &vals[halfint], halfint+1));
+        return outcheck (halfint, search (val, &vals[halfint], halfint + 1));
       else
         return halfint;
-    }
-}
+    }                           // else
+}                               // search
