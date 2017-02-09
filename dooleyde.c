1389b1a0437c201d0035cd0acb2edb0b7339a927
Devin Dooley <devinadooley@gmail.com>
2017-02-08 22:55:13 -0600

    finished search algorithm

--- dooleyde.c
+++ dooleyde.c
@@ -1,14 +1,44 @@
 #include "search.h"
 
 int
-search (int val, int *vals, int n) {
-  int n2 = n/2;
-  if (vals[n2] > val) {
-    search(val, vals, n2);
-  } else if (vals[n2] < val) {
-    search(val, vals, n + n2);
-  } else {
-    return n2; 
+search (int val, int *vals, int n)
+{
+  if (vals[n - 1] == val)
+  {
+    return n - 1; 
   }
+  else if (vals[0] == val)
+  {
+    return 0;
+  }
+  else 
+  {
+    return search_Helper (val, vals, 0, n - 1);
+  }
+}
+
+int
+search_Helper (int val, int *vals, int start, int end)
+{
+  if (start > end)
+  { 
+    return -1;
+  }
+
+  int mid = (start + end) / 2;
+
+  if (vals[mid] > val)
+  {
+    return search_Helper (val, vals, start, mid - 1); 
+  }
+  else if (vals[mid] < val)
+  {
+    return search_Helper (val, vals, mid + 1, end); 
+  }
+  else
+  {
+    return mid;
+  }
+
   return -1;
 }
